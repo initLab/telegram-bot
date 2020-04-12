f6421a87788fafe0b8839af94b8dee331f9ac17d
Vencislav Atanasov <user890104@users.noreply.github.com>
2020-04-12 23:36:12 +0300

    Better formatting of game servers info

--- lib/User890104/HldsStatus.php
+++ lib/User890104/HldsStatus.php
@@ -31,31 +31,17 @@ class HldsStatus {
 		return 'unknown';
 	}
 	
-	private static function formatBoolean($val) {
-		return $val ? 'Yes' : 'No';
+	private static function formatVisibility($val) {
+		return $val ? 'private' : 'public';
 	}
 	
-	private static function getJson($url) {
-		$data = file_get_contents($url);
-		
-		if ($data === false) {
-			throw new Exception('Error getting data');
-		}
-		
-		$data = json_decode($data, true);
-		
-		if ($data === false) {
-			throw new Exception('Error decoding data');
-		}
-		
-		if (array_key_exists('error', $data)) {
-			throw new Exception('Error: ' . $data['error']);
-		}
-		
-		return $data['result'];
+	private static function formatVac($val) {
+		return ($val ? 'with' : 'without') . ' VAC';
 	}
-	
+
 	public static function getStatus($host, $port = 27015) {
+	    $hostAndPort = $host . ':' . $port;
+
         try {
     	    $client = new QueryClient($host, $port, 1, 1);
 
@@ -63,29 +49,29 @@ class HldsStatus {
 			$players = $client->players();
 		}
 		catch (Exception $e) {
-			return '[' . $host . ':' . $port . '] ' . $e->getMessage();
+			return '[' . $hostAndPort . '] ' . $e->getMessage();
 		}
-		
+
 		return
-			'<strong>Host:</strong> ' . $host . ':' . $port . PHP_EOL .
-			'<strong>Game:</strong> ' . $info['Game'] . PHP_EOL .
-			'<strong>Server Name:</strong> ' . $info['Name'] . PHP_EOL .
-			'<strong>Map:</strong> ' . $info['Map'] . PHP_EOL .
-			'<strong>Players:</strong> ' . $info['Players'] . ' / ' . $info['MaxPlayers'] .
-				($info['Bots'] > 0 ? (' (' . $info['Bots'] . ' bots)') : '') . PHP_EOL .
-			'<strong>Server type:</strong> ' . static::formatServerType($info['ServerType']) . PHP_EOL .
-			'<strong>Server OS:</strong> ' . static::formatEnvironment($info['Environment']) . PHP_EOL .
-			'<strong>Public server:</strong> ' . static::formatBoolean($info['Visibility']) . PHP_EOL .
-			'<strong>VAC active:</strong> ' . static::formatBoolean($info['VAC']) . PHP_EOL .
-			'<strong>Version:</strong> ' . $info['Version'] . PHP_EOL .
-			($info['Players'] > 0 ? (PHP_EOL . '<strong>Online players:</strong>' . PHP_EOL . implode(PHP_EOL, array_map(function($player) {
-				return '<strong>' . $player['Name'] . '</strong> (' . $player['Score'] . ' frags)';
-			}, $players))) : '');
+            '[' . $hostAndPort . '] <strong>' . $info['Game'] . '</strong> (' . $info['Name'] .
+            ') at map ' . $info['Map'] . ' with ' . $info['Players'] . ' of ' . $info['MaxPlayers'] .
+            ' players (' . $info['Bots'] . ' bots) (' . static::formatVisibility($info['Visibility']) .
+            ' ' . static::formatServerType($info['ServerType']) . ' server v.' . $info['Version'] .
+            ' on ' . static::formatEnvironment($info['Environment']) . ' ' .
+            static::formatVac($info['VAC']) . ')' .
+            (
+                $info['Players'] > 0 ? (
+                    PHP_EOL . implode(PHP_EOL, array_map(function($player) {
+                        return '#' . $player['Index'] . ' ' . $player['Name'] . ' (' .
+                            $player['Score'] . ' frags)';
+                    }, $players))
+                ) : ''
+            );
 	}
 
 	public static function getStatusMultiple(array $servers)
     {
-        return implode(PHP_EOL, array_map(function($server) {
+        return implode(PHP_EOL . PHP_EOL, array_map(function($server) {
             return static::getStatus($server['host'], $server['port'] ?? 27015);
         }, $servers));
     }
