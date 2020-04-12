27fc3613752023070c8e2b5ca25bbc87229778f7
Vencislav Atanasov <user890104@users.noreply.github.com>
2020-04-12 23:53:33 +0300

    Change formatting

--- lib/User890104/HldsStatus.php
+++ lib/User890104/HldsStatus.php
@@ -4,41 +4,6 @@ namespace User890104;
 use Exception;
 
 class HldsStatus {
-	private static function formatServerType($type) {
-		switch ($type) {
-			case 'd':
-				return 'dedicated';
-			case 'l':
-				return 'non-dedicated';
-			case 'p':
-				return 'SourceTV relay (proxy)';
-		}
-		
-		return 'unknown';
-	}
-	
-	private static function formatEnvironment($type) {
-		switch ($type) {
-			case 'l':
-				return 'Linux';
-			case 'w':
-				return 'Windows';
-			case 'm':
-			case 'o':
-				return 'Mac OS';
-		}
-		
-		return 'unknown';
-	}
-	
-	private static function formatVisibility($val) {
-		return $val ? 'private' : 'public';
-	}
-	
-	private static function formatVac($val) {
-		return ($val ? 'with' : 'without') . ' VAC';
-	}
-
 	public static function getStatus($host, $port = 27015) {
 	    $hostAndPort = $host . ':' . $port;
 
@@ -53,12 +18,9 @@ class HldsStatus {
 		}
 
 		return
-            '[' . $hostAndPort . '] <strong>' . $info['Game'] . '</strong> (' . $info['Name'] .
+            '<pre>' . $hostAndPort . '</pre> <strong>' . $info['Game'] . '</strong> (' . $info['Name'] .
             ') at map ' . $info['Map'] . ' with ' . $info['Players'] . ' of ' . $info['MaxPlayers'] .
-            ' players (' . $info['Bots'] . ' bots) (' . static::formatVisibility($info['Visibility']) .
-            ' ' . static::formatServerType($info['ServerType']) . ' server v.' . $info['Version'] .
-            ' on ' . static::formatEnvironment($info['Environment']) . ' ' .
-            static::formatVac($info['VAC']) . ')' .
+            ' players' . ($info['Bots'] > 0 ? (' (' . $info['Bots'] . ' bots)') : '') .
             (
                 $info['Players'] > 0 ? (
                     PHP_EOL . implode(PHP_EOL, array_map(function($player) {
