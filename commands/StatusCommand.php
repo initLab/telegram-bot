5e9dd44d5a22a3423efd1e0898c3af6b934afa6f
Vencislav Atanasov <git@6bez10.info>
2022-02-13 01:20:20 +0200

    Disable lights status

--- commands/StatusCommand.php
+++ commands/StatusCommand.php
@@ -22,9 +22,9 @@ class StatusCommand extends UserCommand
     protected $usage = '/status';
     protected $version = '1.0.0';
     protected $enabled = true;
-	
+
 	const GUEST_NAME = 'Mystery Labber';
-	
+
 	const UNITS = [
 		'temperature' => '°C',
 		'humidity' => '%',
@@ -36,14 +36,14 @@ class StatusCommand extends UserCommand
 	private static function formatNum($num, $decimals = 0) {
 		return number_format($num, $decimals, '.', '');
 	}
-	
+
 	private static function getJson($url) {
 		$ch = curl_init($url);
-		
+
 		if ($ch === false) {
 			throw new Exception('Unable to create a download manager');
 		}
-		
+
 		if (curl_setopt_array($ch, [
 			CURLOPT_RETURNTRANSFER => true,
 			CURLOPT_CONNECTTIMEOUT => 1,
@@ -52,38 +52,38 @@ class StatusCommand extends UserCommand
 			curl_close($ch);
 			throw new Exception('Unable to set download options');
 		}
-		
+
 		$data = curl_exec($ch);
-		
+
 		$errno = curl_errno($ch);
-		
+
 		if ($errno !== 0) {
 			$error = curl_error($ch);
 			curl_close($ch);
 			throw new Exception($error . ' (code=' . $errno . ')');
 		}
-		
+
 		if ($data === false) {
 			curl_close($ch);
 			throw new Exception('Unable to download data');
 		}
-		
+
 		$code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 		curl_close($ch);
-		
+
 		if ($code >= 400) {
 			throw new Exception('Failed to download the data: HTTP code ' . $code);
 		}
 
 		$data = json_decode($data, true);
-		
+
 		if (is_null($data)) {
 			throw new Exception('Failed to decode the response');
 		}
-		
+
 		return $data;
 	}
-	
+
 	private static function formatDoorStatus($url) {
 		try {
 			$data = static::getJson($url);
@@ -94,7 +94,7 @@ class StatusCommand extends UserCommand
 
 		return 'Door: ' . htmlspecialchars($data['latch']) . ' and ' . htmlspecialchars($data['door']);
 	}
-	
+
 	private static function formatLightsStatus($url) {
 		try {
 			$data = static::getJson($url);
@@ -102,10 +102,10 @@ class StatusCommand extends UserCommand
 		catch (Exception $e) {
 			return 'Lights status not available: ' . $e->getMessage();
 		}
-		
+
 		return 'Lights: ' . htmlspecialchars($data['status']) . ', policy: ' . htmlspecialchars($data['policy']);
 	}
-	
+
 	private static function formatUsers($url) {
 		try {
 			$data = static::getJson($url);
@@ -115,48 +115,48 @@ class StatusCommand extends UserCommand
 		}
 
 		$count = count($data);
-		
+
 		if ($count === 0) {
 			return 'No one in the lab';
 		}
-		
+
 		$result = 'People in the lab: ' . count($data) . PHP_EOL;
 		$guests = 0;
-		
+
 		foreach ($data as $user) {
 			if (is_null($user['id'])) {
 				++$guests;
 				continue;
 			}
-			
+
 			$result .= htmlspecialchars($user['name']) . ' (' . htmlspecialchars($user['username']) . ')';
-			
+
 			if (!empty($user['twitter'])) {
 				$result .= ' <a href="https://twitter.com/' . rawurlencode($user['twitter']) . '">[Twitter]</a>';
 			}
-			
+
 			if (!empty($user['github'])) {
 				$result .= ' <a href="https://github.com/' . rawurlencode($user['github']) . '">[GitHub]</a>';
 			}
-			
+
 			if (!empty($user['url'])) {
 				$result .= ' <a href="' . htmlspecialchars($user['url']) . '">[Web]</a>';
 			}
-			
+
 			$result .= PHP_EOL;
 		}
-		
+
 		if ($guests > 0) {
 			$result .= 'Anonymous labber';
-			
+
 			if ($guests > 1) {
 				$result .= ' x ' . $guests;
 			}
 		}
-		
+
 		return $result;
 	}
-	
+
 	private static function formatMqttStatus(array $config) {
 		try {
 			$data = static::getJson($config['url']);
@@ -164,56 +164,56 @@ class StatusCommand extends UserCommand
 		catch (Exception $e) {
 			return 'MQTT status not available: ' . $e->getMessage();
 		}
-		
+
 		$result = [];
-		
+
 		foreach ($config['sensors'] as $sensor => $sensorData) {
 			$values = [];
-			
+
 			foreach (static::UNITS as $unit => $unitSymbol) {
 				$key = $sensor . '/' . $unit;
-				
+
 				if (!array_key_exists($key, $data)) {
 					continue;
 				}
-				
+
 				$currValue = $data[$key];
 				$label = ucfirst($unit);
 				$delta = max(0, microtime(true) - $currValue['timestamp'] / 1000);
-				
+
 				$message = $label . ': ' . static::formatNum(floatval($currValue['value']), 1) . $unitSymbol;
-				
+
 				if ($delta > $config['timeout']) {
 					$message .= ' (' . static::formatNum($delta) . 's ago)';
 				}
-				
+
 				$values[] = $message;
 			}
-			
+
 			if (count($values) === 0) {
 				continue;
 			}
-			
+
 			$result[] = $sensorData['name'] . ': ' . implode(' / ', $values);
 		}
-		
+
 		return implode(PHP_EOL, $result);
 	}
-	
+
 	private static function isMetadataValid($key, array $dict) {
 		if (!array_key_exists($key, $dict)) {
 			return false;
 		}
-		
+
 		if (in_array($dict[$key], [
 			'<unknown>',
 		])) {
 			return false;
 		}
-		
+
 		return true;
 	}
-	
+
 	private static function formatMusic($url) {
 		try {
 			$data = static::getJson($url);
@@ -221,17 +221,17 @@ class StatusCommand extends UserCommand
 		catch (Exception $e) {
 			return 'Music status not available: ' . $e->getMessage();
 		}
-		
+
 		if (array_key_exists('error', $data)) {
 			return 'Music status error: ' . $data['error'];
 		}
-		
+
 		$states = [
 			'play' => 'playing',
 			'pause' => 'paused',
 			'stop' => 'stopped',
 		];
-		
+
 		return 'Music: [' .
 			$states[$data['status']['state']] . ']' .
 			(array_key_exists('currentSong', $data) ? (
@@ -247,16 +247,16 @@ class StatusCommand extends UserCommand
 				)
 			) : '');
 	}
-	
+
 	private static function getStatus(array $config) {
 		return
 			static::formatDoorStatus($config['door_url']) . PHP_EOL .
-			static::formatLightsStatus($config['lights_url']) . PHP_EOL .
+			//static::formatLightsStatus($config['lights_url']) . PHP_EOL .
 			static::formatMqttStatus($config['mqtt']) . PHP_EOL .
 			static::formatMusic($config['music_url']) . PHP_EOL .
 			static::formatUsers($config['users_url']);
 	}
-	
+
     public function execute()
     {
         $update = $this->getUpdate();
@@ -265,9 +265,9 @@ class StatusCommand extends UserCommand
         $chat_id = $message->getChat()->getId();
 		$message_id = $message->getMessageId();
 		$reply_to_message_id = $chat_id < 0 ? $message_id : null;
-		
+
 		$config = $this->telegram->getCommandConfig('status');
-		
+
 		return Request::sendMessage([
 			'chat_id' => $chat_id,
 			'text' => static::getStatus($config),
